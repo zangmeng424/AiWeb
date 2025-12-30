07f0629ffc7ddfc8e5ef4f9ef320734838b885aa
zangmeng <1561699569@qq.com>
2025-12-30 16:48:45 +0800

    docs: 上传配置文件
    增加配置文件和requirements.txt

---
+++ config_dev.py
@@ -0,0 +1,16 @@
+# mysql
+mysql_host = "127.0.0.1"
+mysql_port = 3306
+mysql_username = "username"
+mysql_password = "password"
+mysql_db = "dbname"
+mysql_pool_size = 10
+
+# redis
+redis_host = "127.0.0.1"
+redis_port = "6379"
+redis_password = "password"
+redis_db = "0"
+
+# 向量模型路径
+embedding = "./bge-small-zh-v1.5"
